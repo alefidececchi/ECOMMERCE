5e7220995e76a2c597803c5fc1df70cf485e84a9
Leandro Buitrago <96851296+LEOBC04@users.noreply.github.com>
2022-07-20 19:09:03 -0500

    Update navBar.jsx

--- client/src/components/NavBar/navBar.jsx
+++ client/src/components/NavBar/navBar.jsx
@@ -1,52 +1,34 @@
 import React from 'react';
 import { Link, Outlet } from 'react-router-dom';
-import style from './navBar.module.scss'
-import cart from '../../assets/imgs/cart.png'
-import user from '../../assets/imgs/user.png'
 
 const NavBar = () => {
   return (
-    <div className={style.container}>
+    <div>
       <section>
         <Outlet />
       </section>
-      <nav  className={style.container}>
+      <nav>
         <ul>
-          <li className={style.nombre}>
-            <Link to={'/home'} className={style.titulo}>
-              NOMBRE DE LA PAGINA
+          <li>
+            <Link to={'/home'}>
+              Home
             </Link>
           </li>
-          <li className={style.all}>
-            <Link to={'/user'}className={style.linkAll}>
-              All
-            </Link>
-          </li>
-          <li className={style.today}>
-              <Link to={'/wish'} className={style.items}>
-                Today's Deals
+            <li>
+              <Link to={'/shopping'}>
+                Cart
               </Link>
-          </li>
-          <li className={style.giftCard}>
-              <Link to={'/wish'} className={style.items}>
-                Gift Card
+            </li>
+            <li>
+              <Link to={'/user'}>
+                User
               </Link>
-          </li>
-          <li className={style.cart}>
-            <Link to={'/shopping'}>
-              <img src={cart} alt='cart' className={style.images}/>
-            </Link>
-          </li>
-          <li className={style.user} >
-            <Link to={'/user'}>
-              <img src={user} alt='cart' className={style.images}/>
-            </Link>
-          </li>
-          <li className={style.wishList}>
-            <Link to={'/wish'} className={style.items}>
-              WishList
-            </Link>
-          </li>
+            </li>
+            <li>
+              <Link to={'/wish'}>
+                WishList
+              </Link>
+            </li>
         </ul>
       </nav>
     </div>
