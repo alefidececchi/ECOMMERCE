11bce249bb0b68d865790d390f3fdfed073e12c9
DESKTOP-9VHM372\Felipe <felipebravo-96@hotmail.com>
2022-07-20 19:04:55 -0500

    For  para edicion

---
+++ client/src/components/EditBooks/editBook.jsx
@@ -0,0 +1,211 @@
+import { useState } from "react";
+import {Formik, Form,  Field, ErrorMessage} from 'formik'
+import {useNavigate} from "react-router-dom";
+import style from './editBook.module.scss'
+
+
+function EditBook({editOff}){
+
+    const [send, setSend] = useState(false);
+    const[editMode, setEditMode] = useState(true)
+    
+    let navigate  = useNavigate()
+
+
+
+
+    return(
+        <div>
+ 
+                    <section>
+                    <Formik
+                        initialValues={{
+                            bookName:"", 
+                            image:"", 
+                            price:'', 
+                            amount:'',
+                            published:'', 
+                            genre:'',
+                            language:'',
+                            state:''
+                        }}
+                        validate={(values) =>{
+                            let errors ={};
+                            //validacion nombre del libro   
+                            if(!values.bookName){
+                                errors.bookName = 'Please write the book name'
+                            }else if(values.bookName.length < 4 || values.bookName.length > 20){
+                                errors.bookName = 'Book name must have between 4 or 20 characters'
+                            }
+
+                            //validacion año publicacion
+                            if(!values.published){
+                                errors.published = 'Please write the published year of the book'
+                            }else if(values.published < 0){
+                                errors.published = 'The year must be possitive'
+                            }
+                        
+                            //validacion genero
+                            if(!values.genre){
+                                errors.genre = 'Please write the genre of the book'
+                            }
+
+                            //validacion language
+                            if(!values.language){
+                                errors.language = 'Please select the language of the book'
+                            }else if(values.language === 'Language'){
+                                errors.language = 'Please select the language of the book'
+                            }
+
+                            //validacion imagen
+                            if(!values.image){
+                                errors.image = 'Please enter the book image link'
+                            }
+                            else if (
+                                !/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#()?&//=]*)/.test(
+                                    values.image //como agregar imagenes delcomputador restringiendo formatos jgp png jpeg
+                                )
+                            ) {
+                                errors.image = "Please insert a valid URL";
+                            }
+                            //validacion precio  
+                            if(!values.price){
+                                errors.price = 'Please write the book price'
+                            }else if(values.price < 0){
+                                errors.price = 'The price must be possitive'
+                            }
+
+                            //validacion cantidad 
+                            if(!values.amount){
+                                errors.amount = 'Please write the book amount for selling'
+                            }else if(values.amount < 0){
+                                errors.amount = 'The amount must be possitive'
+                            }
+                            //validacion estado
+                            if(!values.state){
+                                errors.state = 'Please select the book state'
+                            }
+                            return errors;
+                        }}
+                        onSubmit={(values, {resetForm})=>{
+                            resetForm();
+                            setSend(true)
+                            setTimeout(() => setSend(false), 3000)
+                            console.log(values)
+                        }}
+                    >
+                        {( {errors} )=>(
+                            <Form> 
+                            <h2>Post new book</h2>
+                            <div>
+                                <label htmlFor="name">Book Name: </label>
+                                <Field
+                                type='text'
+                                id="name"
+                                placeholder="Type a name..."
+                                name="bookName"
+                                />
+                                <ErrorMessage name="bookName" component={()=>(
+                                    <div>{errors.bookName}</div>
+                                )} />
+                            </div>
+                            <div>
+                                <label htmlFor="name">Published Year: </label>
+                                <Field
+                                type='number'
+                                id="published"
+                                placeholder="1996"
+                                name="published"
+                                />
+                                <ErrorMessage name="published" component={()=>(
+                                    <div>{errors.published}</div>
+                                )} />
+                            </div>
+                            <div>
+                                <label htmlFor="name">Genre: </label>
+                                <Field
+                                type='text'
+                                id="genre"
+                                placeholder="Terror..."
+                                name="genre"
+                                />
+                                <ErrorMessage name="genre" component={()=>(
+                                    <div>{errors.genre}</div>
+                                )} />
+                            </div>
+                            <div>
+                                <label htmlFor="img">Image: </label>
+                                <Field
+                                    type="text"
+                                    id="img"
+                                    placeholder="https://image-url-png"
+                                    name="image"
+                                />
+                                <ErrorMessage name="image" component={()=>(
+                                    <div >{errors.image}</div>
+                                )} />
+                            </div>
+                            <div>
+                                <label htmlFor="prc">Price: </label>
+                                <Field
+                                    type="number"
+                                    id="prc"
+                                    placeholder="$9.50"
+                                    name="price"
+                                />
+                                <ErrorMessage name="price" component={()=>(
+                                    <div>{errors.price}</div>
+                                )} />
+                            </div>
+                            <div>
+                                <label htmlFor="amt">Stock: </label>
+                                <Field
+                                    type="number"
+                                    id="amt"
+                                    placeholder="15"
+                                    name="amount"
+                                />
+                                <ErrorMessage name="amount" component={()=>(
+                                    <div>{errors.amount}</div>
+                                )} />
+                
+                            </div>
+                            <div>
+                                <label htmlFor="name">Language: </label>
+                                <Field id="language"  name="language" as='select'>
+                                    <option value='Language' selected> Language </option>
+                                    <option value='English'> English </option>
+                                    <option value='Spanish'> Spanish </option>    
+                                </Field>                
+                                <ErrorMessage name="language" component={()=>(
+                                    <div>{errors.language}</div>
+                                )} />
+                            </div>
+                            <div>
+                                <label>State </label>
+                                <label>
+                                    <Field  type='radio' name="state" value='New'/> New
+                                </label>
+                                <label>
+                                    <Field  type='radio' name="state" value='Old'/> Secondhand
+                                </label>
+                                <ErrorMessage name="state" component={()=>(
+                                    <div>{errors.state}</div>
+                                )} />
+                            </div>
+                            <div>
+                                <button type="submit">  Create </button>
+                                {send && <p>Product added succecsfully</p>}
+                            </div>  
+                        </Form>
+                        )}
+                    </Formik>
+                    <div>
+                        <button onClick={() => editOff()}>  Cancel </button>
+                    </div>  
+                </section>
+        </div>
+    )
+}
+
+export default EditBook;
--- client/src/components/User/user.jsx
+++ client/src/components/User/user.jsx
@@ -1,43 +1,62 @@
 import React from 'react';
 import { Link, Outlet } from 'react-router-dom';
 import style from './user.module.scss'
-import userLogo from '../../assets/imgs/user.png'
+
 import { useState } from 'react';
+import EditBook from '../EditBooks/editBook';
+
+import userLogo from '../../assets/imgs/user.png'
+import portada from '../../assets/imgs/hp.jpg'
 
 const User = () => {
   
   const[image, setImage] = useState(userLogo)
+  const[editMode, setEditMode] = useState(false)
   //const[books, setBooks] = useState([{    bookName:"Harry Popote", image:"No disponible :V", price: 40.50, amount: 15, state:'Nuevesito prro'}])
 
-   let books = false
+  let books = false
   //  let books = [{    bookName:"Harry Popote", 
   //   image:"No disponible :V", 
   //   price: 40.50, 
   //   amount: 15, 
-  //   state:'Nuevesito prro'}]
+  //   state:'Nuevesito prro'},
+  //   // {    bookName:"Harry Popote 2", 
+    // image:"No disponible :V", 
+    // price: 40.50, 
+    // amount: 15, 
+    // state:'Nuevesito prro'},
+  //]
 
 
 
 
-  function deleteBook(book){
+  function deleteBook(){
     console.log('Borrado')
+    console.log(books)
+    books = []
+    console.log(books)
+
   }
 
 
   function onInputchange(e){
-    e.preventDefault()
     console.log( e.target.value)
-    let url = URL.createObjectURL(e.target);
+    let url = URL.createObjectURL(e.target.files[0]);
+    console.log(url)
+    setImage(url)
+  }
 
-    setImage({
-      ...image,
-      [e.target.name]:  url
-    })
+  function editOn(){
+    setEditMode(true)
+  }
+
+  const editOff = () =>{
+    setEditMode(false)
   }
 
   return (
-    <div>
-      <div className={style.maincontainer}>
+    <div className={style.mainContainer}>
+      <div className={style.containerProfile}>
         <img src={image} className={style.userPhoto} alt='userProfile'></img>
         <h2>Usuario Nombre</h2>
         <form encType='multipart/form-data'>
@@ -56,27 +75,34 @@ const User = () => {
         </div>
       </div>
       <div className={style.addBook}>
-          {
+          { 
+            editMode?(
+              <div>
+                <EditBook editOff={editOff}/>
+              </div>
+            ):
             books.length > 0 ?
             books.map((book , i) =>
               <div>
                   <div key={i}>
                     <h1> {book.bookName}</h1>
-                    <img src={book.image} alt='bookImage'></img>
+                    <img src={portada} alt='bookImage'></img>
                     <h3> Price:$ {book.price} USD</h3>
                     <h3> Amount: {book.amount}</h3>
                     <h3> State: {book.state}</h3>
                   </div>
                   <div>
-                    <button className={style.button}>Edit</button>
+                    <button className={style.button} onClick={editOn}>Edit</button>
                   </div>
                   <div>
-                    <button className={style.button} onClick={()=>deleteBook(book)}>X</button>
+                    <button className={style.button} onClick={deleteBook}>Delete</button>
                   </div>
               </div>
               
               ):
               <div>
+                <h2>Hi!</h2>
+                <h2>Do you want to sell a book?</h2>
                 <Link to={'/user/newBook'}>
                   <button className={style.button}>Post a Book</button>
                 </Link>
@@ -88,8 +114,3 @@ const User = () => {
 }
 
 export default User;
-// bookName:"", 
-// image:"", 
-// price:'', 
-// amount:'', 
-// state:'
