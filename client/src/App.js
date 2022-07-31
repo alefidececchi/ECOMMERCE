5ea690cab0a2eb7f3b5ac57b383fb1183cb2021b
Diego <[diego.carcaustor@gmail.com]>
2022-07-31 18:24:35 -0500

    carpeta cliente original sin cambios

--- client/src/App.js
+++ client/src/App.js
@@ -7,39 +7,26 @@ import Shopping from './components/Shopping/shopping.jsx';
 import User from './components/User/user.jsx';
 import WishList from './components/WishList/wishList.jsx';
 import AddBook from './components/BookForm/addBook.jsx'
-import Register from "./components/Register/register"
-import Login from './components/Register/login';
-import Activate from "./components/Register/activate"
-import Forgot from "./components/Register/forgot"
-import Reset from "./components/Register/reset"
 import { ToastContainer } from 'react-toastify'
 
 
+
 function App() {
-  
   return (
     <div className="App">
-      <ToastContainer />
-      <Routes>
-
-        <Route path='/' element={< NavBar />}>
-          <Route path="/" element={< Home />} />
-          <Route path='shopping' element={< Shopping />} />
-          <Route path='user' element={< User />} />
-          <Route path='user/newBook' element={< AddBook />} />
-          <Route path='wish' element={< WishList />} />
-
-        </Route>
-
-        <Route path='register' element={< Register />} />
-        <Route path='activate-account' element={< Activate />} />
-        <Route path='activate-account/:token' element={< Activate/>} />
-        <Route path='login' element={< Login />} />
-        <Route path='forgot-password' element={< Forgot />} />
-        <Route path='reset-password/:token' element={< Reset />} />
-      </Routes>
+    <ToastContainer />
+     <Routes>
+     
+      <Route path='/' element={< NavBar />}>
+        <Route path="/" element={< Home />} />
+        <Route path='shopping' element={< Shopping />} />
+        <Route path='user' element={< User />} />
+        <Route path='user/newBook' element={< AddBook />} />
+        <Route path='wish' element={< WishList />} />
+      </Route>
+     </Routes>
     </div>
-  );
+  ); 
 }
 
 export default App;
--- client/src/Redux/store/store.js
+++ client/src/Redux/store/store.js
@@ -2,16 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 import booksReducer from "../slices/bookSlice";
 import shoppingCartReducer, { getTotals } from '../slices/shoping.slice';
 import genreReducer from '../slices/genreSlice';
-import userReducer from '../slices/userSlice';
-import tokenReducer from '../slices/tokenSlice';
+
 
 const store = configureStore({
   reducer: {
     books: booksReducer,
     shoppingCart: shoppingCartReducer,
     genres: genreReducer,
-    users:userReducer,
-    token:tokenReducer,
   }
 })
 store.dispatch(getTotals())
