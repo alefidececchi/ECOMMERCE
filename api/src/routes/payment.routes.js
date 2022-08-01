881784cb0635c2bf13a2943d74e16f8a612516d6
Daniela Camargo <daniela.camargopr@gmail.com>
2022-08-01 12:20:07 -0500

    try deploy

--- api/src/routes/payment.routes.js
+++ api/src/routes/payment.routes.js
@@ -12,28 +12,28 @@ router.post('/create-checkout-session', payment);
 
 
 // This is your Stripe CLI webhook secret for testing your endpoint locally.
-// const endpointSecret = "whsec_589c8a4742cefef3fe36e18f0a2d9f7fabb41d23c3b86f32832e6842360d7eea";
+const endpointSecret = "whsec_589c8a4742cefef3fe36e18f0a2d9f7fabb41d23c3b86f32832e6842360d7eea";
 
-// router.post('/webhook', express.raw({type: 'application/json'}), (request, response) => {
-//   const sig = request.headers['stripe-signature'];
+router.post('/webhook', express.raw({type: 'application/json'}), (request, response) => {
+  const sig = request.headers['stripe-signature'];
 
-//   let event;
+  let event;
 
-//   try {
-//     event = stripe.webhooks.constructEvent(request.body, sig, endpointSecret);
-//     console.log("Webhook verified")
-//   } catch (err) {
-//     console.log(`Webhook Error: ${err.message}`)
-//     response.status(400).send(`Webhook Error: ${err.message}`);
-//     return;
-//   }
+  try {
+    event = stripe.webhooks.constructEvent(request.body, sig, endpointSecret);
+    console.log("Webhook verified")
+  } catch (err) {
+    console.log(`Webhook Error: ${err.message}`)
+    response.status(400).send(`Webhook Error: ${err.message}`);
+    return;
+  }
 
-//   // Handle the event
+  // Handle the event
   
 
-//   // Return a 200 response to acknowledge receipt of the event
-//   response.send().end();
-// });
+  // Return a 200 response to acknowledge receipt of the event
+  response.send().end();
+});
 
 
 
