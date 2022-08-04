c1af7c7655e491f2dbcbe937c4cfc5a7862b6239
Daniela Camargo <daniela.camargopr@gmail.com>
2022-08-04 09:20:27 -0500

    style-dicount

--- client/src/components/Shopping/PayButton.jsx
+++ client/src/components/Shopping/PayButton.jsx
@@ -1,29 +1,35 @@
 import axios from 'axios'
-import {useSelector} from 'react-redux'
+import { useSelector } from 'react-redux'
 import { url } from '../../Redux/slices/api'
 import { toast } from 'react-toastify'
+import { Navigate, useNavigate } from 'react-router-dom'
 
-const PayButton = ({cartItems}) =>{
+const PayButton = ({ cartItems }) => {
 
+    let navigate = useNavigate()
     const user = useSelector((state) => state.auth)
 
     const handleCheckout = () => {
         console.log(user.token)
-        if(user.token){
-        axios.post(`${url}/stripe/create-checkout-session`, {
-            cartItems,
-            userId: user._id
-        }).then((res) =>{
-            if(res.data.url){
-                window.location.href = res.data.url
-            }
-        }).catch((err) => console.log(err.message))}
-        else{
-            toast.error(`First you must be login`, {position: "top-center"})
+        if (user.token) {
+            axios.post(`${url}/stripe/create-checkout-session`, {
+                cartItems,
+                userId: user._id
+            }).then((res) => {
+                if (res.data.url) {
+                    window.location.href = res.data.url
+                }
+            }).catch((err) => console.log(err.message))
         }
+        else {
+            navigate('/login')
+        }
+
+        // toast.error(`First you must be login`, {position: "top-center"})
+
     }
 
-    return(
+    return (
         <div>
             <button onClick={() => handleCheckout()}>Check out</button>
         </div>
