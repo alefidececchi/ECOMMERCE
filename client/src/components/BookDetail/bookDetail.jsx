a1173bc981d96b2e6acb9370b62fbc8f414ad933
Daniela Camargo <daniela.camargopr@gmail.com>
2022-08-07 10:56:55 -0500

    review and google working

--- client/src/components/BookDetail/bookDetail.jsx
+++ client/src/components/BookDetail/bookDetail.jsx
@@ -25,8 +25,46 @@ const BookDetail = (props, { book }) => {
 
 
     var { booksDetail } = useSelector((state) => state.books)
-    // console.log(booksDetail.name)
-    // console.log(props)
+    // console.log(booksDetail.reviews)
+
+
+    let comment, score
+    var reviews = booksDetail.reviews
+
+    if (reviews) { //una vez ya se hayan traido los datos renderizalos
+        comment = reviews.length == 1 ? reviews[0].comment : reviews[0].comment + ", " + reviews[1].comment
+        score = reviews.length == 1 ? reviews[0].score : (reviews[0].score + reviews[1].score) / 2
+
+    }
+
+    // console.log(comment)
+
+
+    // var review = booksDetail.reviews
+    //  const x = review.review[0]
+    //    const x=review.find(elm=>elm[0].score)
+    // console.log(review)
+    // console.log(review.find(obj=>obj.comment))
+    // var array = review[0].comment
+    // console.log(array)
+
+    // var reviews = booksDetail.reviews.map(elm => {
+    //     return (
+    //         elm.comment
+    //     )
+
+    // }).join(", ")
+    // console.log(reviews)
+    // let name
+    // if (booksDetail[0]) { //una vez ya se hayan traido los datos renderizalos
+    // name = booksDetail[0].name;}
+    // img = detailDog[0].img;
+    // temperament = detailDog[0].temperament
+    // height = detailDog[0].height;
+    // weight = detailDog[0].weight;
+    // life_span = detailDog[0].life_span;
+
+
 
 
     const [close, setClose] = useState(false);
@@ -69,11 +107,13 @@ const BookDetail = (props, { book }) => {
                     ) :
                         <div className={s.BookDetail}>
 
-                            <button className={s.fav} onClick={() => handleWishList(book)}>
-                                <i class="fas fa-heart"></i>
-                            </button>
+
 
                             <div>
+
+                                <button className={s.fav} onClick={() => handleWishList(book)}>
+                                    <i class="fas fa-heart"></i>
+                                </button>
                                 <button className={s.Close} onClick={handleClose} >
                                     <i class="fas fa-times fa-lg"></i>
                                 </button>
@@ -95,12 +135,18 @@ const BookDetail = (props, { book }) => {
 
                                         <h4>{booksDetail.description}</h4>
 
+
+                                        <h4>Score: {score}</h4>
+                                        <h4>Comment: {comment}</h4>
+
                                         <figure onClick={handleClick} >
                                             <button>
                                                 <i class="fas fa-cart-plus fa-lg"></i>  <h3>Add to cart</h3>
                                             </button>
                                         </figure>
-                                    </div> </div></div>
+                                    </div>
+                                </div>
+                            </div>
 
 
                         </div>} </div>
